7995b24e4e6d4d9bde340fb5015ea10f8f933797
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-11-22 16:47:41 +0100

    Basic NFT: Deploy Script

---
+++ script/DeployAcidNft.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {AcidNft} from "../src/AcidNft.sol";
+
+contract DeployScriptNft is Script {
+    function run() external returns (AcidNft acidNft) {
+        vm.startBroadcast();
+        acidNft = new AcidNft();
+        vm.stopBroadcast();
+    }
+}
